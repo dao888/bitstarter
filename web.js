aa04ff0e3502d2513563c5bec40a3704fbb8bcf9
Ubuntu <ubuntu@ip-172-31-21-151.us-west-2.compute.internal>
2013-07-16 03:03:17 +0000

    fix buffer

--- web.js
+++ web.js
@@ -7,14 +7,14 @@ var app = express.createServer(express.logger());
 
 
 
-<!--var buffer = new Buffer(fs.readFileSync('index.html'), "utf-8");-->
+var buffer = fs.readFileSync('index.html', 'utf8')
 
 
 
 
 
 app.get('/', function(request, response) {
-  response.send('Hello buffer!');
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
